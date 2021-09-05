8e5c3fc1e787c6085500c42259fa165906bf75bc
Rjkzavr <kolya.toropof@gmail.com>
2021-09-05 14:01:26 +0300

    сделал добавление/удаление/редактирование админа
    вынес проверку пароля в отдельную функцию
    сделал небольшой ренейминг названия типов ссылок
    функция регистрации используется при создании и админа и пользователя (передаётся параметр доступа)
    небольшой редизайн админской панели

--- data/form.py
+++ data/form.py
@@ -1,17 +1,38 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, TextAreaField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Email, EqualTo
-from wtforms import *
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+
+
+class YesNoFrom(FlaskForm):
+    cancel = SubmitField("Нет")
+    submit = SubmitField("Да")
 
 
 class StartForm(FlaskForm):
     submit = SubmitField("Готово")
 
 
+class AdminForm(FlaskForm):
+    cancel = SubmitField("Отмена")
+    submit = SubmitField("Применить")
+
+
 class UserForm(StartForm):
     name = StringField('Логин', validators=[DataRequired("Пожалуйста введите Логин")])
-    email = StringField('Почту', validators=[DataRequired("Пожалуйста введите Почту"), Email("Пожалуйста корректную Почту")])
+    email = StringField('Почту',
+                        validators=[DataRequired("Пожалуйста введите Почту"), Email("Пожалуйста корректную Почту")])
     first_password = PasswordField('Введите пароль', validators=[DataRequired("Пожалуйста введите Пароль")])
-    repeat_password = PasswordField('Повторите пароль', validators=[DataRequired("Пожалуйста повторите Пароль"), EqualTo('first_password', message='Пароли не совпадают')])
+    repeat_password = PasswordField('Повторите пароль', validators=[DataRequired("Пожалуйста повторите Пароль"),
+                                                                    EqualTo('first_password',
+                                                                            message='Пароли не совпадают')])
     remember = BooleanField("Запомнить меня")
     password = PasswordField('Пароль', validators=[DataRequired("Пожалуйста введите Пароль")])
+
+
+class ChangeAminForm(AdminForm):
+    name = StringField('Логин', validators=[DataRequired("Пожалуйста введите Логин")])
+    email = StringField('Почту',
+                        validators=[DataRequired("Пожалуйста введите Почту"), Email("Пожалуйста корректную Почту")])
+    first_password = PasswordField('Новый пароль', validators=[])
+    repeat_password = PasswordField('Повторите пароль', validators=[EqualTo('first_password',
+                                                                            message='Пароли не совпадают')])
--- main.py
+++ main.py
@@ -17,21 +17,37 @@ data = {
         ['/', 'главная'],
         ['/item/1', 'товар номер 1'],
     ],
-    "logined_links": [
+    "authorized_links": [
         ['/logout', 'разлогиниться'],
     ],
-    "unlogined_links": [
-        ['/login', 'зaлогиниться'],
+    "unauthorized_links": [
+        ['/login', 'зарегестрироваться'],
     ],
     "admin_links": [
         ['/admin', 'панель администратора'],
     ],
+    "admin_page": [
+        ['/admin', 'главная']
+    ],
 }
+
+
+def check_passwords(password_1, password_2):
+    error = ""
+    if len(password_1) < 8:
+        error = "Длинна пароля менее 8 символов"
+    elif password_1.isdigit():
+        error = "В пароле нет буквы"
+    elif password_1.isalpha():
+        error = "В пароле нет цифры"
+    elif password_1 != password_2:
+        error = "Пароли не совпадают"
+    return error
 
 
 def render_page(page: str, title, **kwargs):
     return render_template(page, title=title, data=data,
-                           is_logined=not current_user.is_anonymous,
+                           authorized=not current_user.is_anonymous,
                            is_admin=not current_user.is_anonymous and current_user.access > 0,
                            **kwargs)
 
@@ -70,25 +86,20 @@ def login_page():
     return render_page('users/login.html', 'Логин', form=form, error=error)
 
 
-def register(register_form):
+def register(register_form, access):
     error = ""
     session = db_session.create_session()
     if session.query(User).filter(User.email == register_form.email.data).first():
         error = "Эта почта уже использовалась"
     elif session.query(User).filter(User.nickname == register_form.name.data).first():
         error = "Уже есть пользователь с таким именем"
-    elif register_form.first_password.data != register_form.repeat_password.data:
-        error = "Пароли не совпадают"
-    elif len(register_form.first_password.data) < 8:
-        error = "Длинна пароля менее 8 символов"
-    elif register_form.first_password.data.isdigit():
-        error = "В пароле нет буквы"
-    elif register_form.first_password.data.isalpha():
-        error = "В пароле нет цифры"
+    elif check_passwords(register_form.first_password.data, register_form.repeat_password.data):
+        error = check_passwords(register_form.first_password.data, register_form.repeat_password.data)
     else:
         new_user = User()
         new_user.nickname = register_form.name.data
         new_user.email = register_form.email.data
+        new_user.access = access
         new_user.set_password(register_form.first_password.data)
         session.add(new_user)
         session.commit()
@@ -101,7 +112,7 @@ def register_page():
     form = UserForm()
     error = ""
     if request.method == 'POST':
-        error = register(form)
+        error = register(form, 0)
         if error == "":
             return redirect('/login')
     return render_page('users/register.html', 'Регистрация', form=form, error=error)
@@ -114,13 +125,115 @@ def logout_page():
     return redirect("/")
 
 
+# admins page
+def upward_admin(current_admin, id_admin):
+    session = db_session.create_session()
+    searched_admin = session.query(User).filter(User.id == id_admin).first()
+    session.close()
+    return current_admin.access > searched_admin.access
+
+
+def get_admin(id_admin):
+    session = db_session.create_session()
+    searched_admin = session.query(User).filter(User.id == id_admin).first()
+    session.close()
+    return searched_admin
+
+
+def admin():
+    session = db_session.create_session()
+    admins = session.query(User).filter(User.access > 0).all()
+    session.close()
+    return admins
+
+
 @app.route('/admin/')
 @login_required
 def admin_page():
-    if current_user.access > 0:
-        return render_page('admin/main.html', 'Панель администратора', admin=current_user)
-    else:
+    if current_user.access < 1:
         return redirect('/login')
+    admins = admin()
+    return render_page('admin/main.html', 'Панель администратора', current_admin=current_user, admins=admins)
+
+
+@app.route('/admin/admin/register/', methods=['GET', 'POST'])
+def admin_register_page():
+    form = UserForm()
+    error = ""
+    if request.method == 'POST':
+        error = register(form, 1)
+        if error == "":
+            return redirect('/admin')
+    return render_page('users/register.html', 'Регистрация', form=form, error=error)
+
+
+def admin_change(id_admin, change_admin_from):
+    error = ""
+    session = db_session.create_session()
+    searched_admin = session.query(User).filter(User.id == id_admin).first()
+    if (session.query(User).filter(User.email == change_admin_from.email.data).first() and
+            change_admin_from.email.data != searched_admin.email):
+        error = "Эта почта уже использовалась"
+    elif (session.query(User).filter(User.nickname == change_admin_from.name.data).first() and
+          change_admin_from.name.data != searched_admin.nickname):
+        error = "Уже есть пользователь с таким именем"
+    elif (check_passwords(change_admin_from.first_password.data, change_admin_from.repeat_password.data) and
+          change_admin_from.first_password.data != ""):
+        error = check_passwords(change_admin_from.first_password.data, change_admin_from.repeat_password.data)
+    else:
+        searched_admin.nickname = change_admin_from.name.data
+        searched_admin.email = change_admin_from.email.data
+        if change_admin_from.first_password.data != "":
+            searched_admin.set_password(change_admin_from.first_password.data)
+        session.commit()
+    session.close()
+    return error
+
+
+@app.route('/admin/admin/change/<int:id_admin>', methods=['GET', 'POST'])
+@login_required
+def admin_change_page(id_admin):
+    if current_user.access < 1:
+        return redirect('/login')
+    if not upward_admin(current_user, id_admin):
+        return redirect('/admin')
+    form = ChangeAminForm()
+    error = ""
+    if request.method == 'POST':
+        if form.submit.data:
+            error = admin_change(id_admin, form)
+            if error == "":
+                return redirect('/admin')
+        else:
+            return redirect('/admin')
+    else:
+        searched_admin = get_admin(id_admin)
+        form.name.data = searched_admin.nickname
+        form.email.data = searched_admin.email
+    return render_page('admin/admin/change.html', 'Изменить админа', form=form, admin=get_admin(id_admin), error=error)
+
+
+def admin_delete(id_admin):
+    session = db_session.create_session()
+    searched_admin = session.query(User).filter(User.id == id_admin).first()
+    session.delete(searched_admin)
+    session.commit()
+    session.close()
+
+
+@app.route('/admin/admin/delete/<int:id_admin>', methods=['GET', 'POST'])
+@login_required
+def admin_delete_page(id_admin):
+    if current_user.access < 1:
+        return redirect('/login')
+    if not upward_admin(current_user, id_admin):
+        return redirect('/admin')
+    form = YesNoFrom()
+    if request.method == 'POST':
+        if form.submit.data:
+            admin_delete(id_admin)
+        return redirect('/admin')
+    return render_page('admin/admin/delete.html', 'Удалить админа', form=form, admin=get_admin(id_admin))
 
 
 @app.route('/')
